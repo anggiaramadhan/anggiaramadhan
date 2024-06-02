0ea9d095ff4978caded33aeccd92de28025f1a1b
anggiariksa <anggia@tokyotreat.com>
2024-06-02 07:15:03 +0700

    update meta desc

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Anggia Ramadhan's Blog",
   shortTitle: "Anggia Riksa Ramadhan",
   description:
-    "Doa ibu merupakan personal blog mengenai teknologi dan apa saja",
+    "Anggia Ramadhan's blog merupakan personal blog mengenai teknologi dan apa saja",
   url: "https://anggiaramadhan.life/",
   author: {
     name: "Anak Ibu",
